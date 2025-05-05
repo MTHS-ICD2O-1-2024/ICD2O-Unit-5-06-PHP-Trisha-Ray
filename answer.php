9733fa8c9d7b6cae8b5fdc84e2e60314b1363e56
default <trisha.ray@stu.ocsb.ca>
2025-05-05 17:05:31 +0000

    multiplication fixed

---
+++ answer.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!-- ICS2O-Unit2-04-HTML-MDL -->
+<html lang="en-ca">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Multiplication with while loops, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Trisha Ray" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="./apple-touch-icon.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="./favicon-32x32.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>Multiplication with while loops, in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Multiplication with while loops, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/loop.png" alt="loop" />
+      </div>
+      <div class="page-content-php">
+        <div id="user-info">
+          <?php
+          $firstNumber = $_GET["numberone"];
+          $secondNumber = $_GET["numbertwo"];
+          $answer = 0;
+
+          // process
+          while ($firstNumber >= 1) {
+            $answer = $answer + $secondNumber;
+            $firstNumber = $firstNumber - 1;
+          }
+          // output
+          echo "If the first number is " . $firstNumber . "and the second number is " . $secondNumber;
+          echo "The number is: " . $answer;
+          ?>
+        </div>
+        <div class="page-content-answer">
+          <a href="./index.php">Return...</a>
+        </div>
+      </div>
+    </main>
+  </div>
+</body>
+
+</html>
